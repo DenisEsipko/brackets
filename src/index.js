fa60f8ea8473d6913c28271cbb95f897ddeecbfd
DenisEsipko <deni007a1@gmail.com>
2021-08-09 15:41:22 +0300

    init: Finished the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =
+function check(str, bracketsConfig) {
+
+    const STACK = [];
+    // создаем Map ключь  открывающая скобка значение закрывающая
+    const brackets = {};
+    bracketsConfig.forEach((x) => (brackets[x[0]] = x[1]));
+    //
+
+    for (const strElement of str) {
+        if (brackets[STACK[STACK.length - 1]] === strElement) {
+            STACK.pop();
+        } else if (brackets.hasOwnProperty(strElement)) {
+            STACK.push(strElement);
+        } else {
+            return false;
+        }
+    }
+
+    if (STACK.length === 0) {
+        return true;
+    }
+    return false;
 }
+
+
+
+
+
